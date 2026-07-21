0299c43d006207d442dd1f961dd11f62f196f208
yisus <yisus@JESUSGNZ>
2026-07-20 23:59:49 -0600

    [ADD] Endpoint Obtener Productos con Paginado

---
+++ src/Catalog.API/Models/Products/GetProductPag/GetProductPagEndPoint.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.API.Models.Products.GetProducts;
+
+namespace Catalog.API.Models.Products.GetProductPag
+{
+    public record GetProductsPagResponse(IEnumerable<Product> Products, int TotalCount, int PageNumber, int PageSize);
+    public class GetProductPagEndPoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products/page", async (ISender sender, int pageNumber = 1, int pageSize = 10) =>
+            {
+                var result = await sender.Send(new GetProductsPagQuery(pageNumber, pageSize));
+                return Results.Ok(result.Adapt<GetProductsPagResponse>());
+            })
+            .WithName("ObtenerProductos")
+            .Produces<GetProductsPagResponse>(StatusCodes.Status200OK)
+            .WithSummary("Obtener Productos Paginados")
+            .WithDescription("Obtiene todos los productos con paginación");
+        }
+    }
+}
---
+++ src/Catalog.API/Models/Products/GetProductPag/GetProductPagQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using Catalog.API.Models.Products.GetProducts;
+using Marten.Pagination;
+
+namespace Catalog.API.Models.Products.GetProductPag
+{
+    public record GetProductsPagQuery(int PageNumber = 1, int PageSize = 10) : IQuery<GetProductsPagResult>;
+    public record GetProductsPagResult(IEnumerable<Product> Products, int TotalCount, int PageNumber, int PageSize);
+    public class GetProductPagQueryHandler(IDocumentSession session) : IQueryHandler<GetProductsPagQuery, GetProductsPagResult>
+    {
+        public async Task<GetProductsPagResult> Handle(GetProductsPagQuery query, CancellationToken cancellationToken)
+        {
+            var totalCount = await session.Query<Product>().CountAsync(cancellationToken);
+
+            var products = await session.Query<Product>()
+                .ToPagedListAsync(query.PageNumber, query.PageSize, cancellationToken);
+
+            return new GetProductsPagResult(products, totalCount, query.PageNumber, query.PageSize);
+        }
+    }
+}
