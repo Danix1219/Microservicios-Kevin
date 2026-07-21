6b22835338143b93cb6ccae2207b2da65fed6e29
yisus <yisus@JESUSGNZ>
2026-07-20 22:20:33 -0600

    [ADD] Endpoint Busqueda de Producto por Nombre

---
+++ src/Catalog.API/Models/Products/GetProductsByName/GetProductsByNameCommandHandler.cs
@@ -0,0 +1,16 @@
+﻿namespace Catalog.API.Models.Products.GetProductsBy
+{
+    public record GetProductByNameQuery(string Name) : IQuery<GetProductByNameResult>;
+    public record GetProductByNameResult(IEnumerable<Product> Products);
+    public class GetProductsByNameCommandHandler(IDocumentSession session) : IQueryHandler<GetProductByNameQuery, GetProductByNameResult>
+    {
+        public async Task<GetProductByNameResult> Handle(GetProductByNameQuery query, CancellationToken cancellationToken)
+        {
+            var products = await session.Query<Product>()
+                .Where(p => p.Name.ToLower().Contains(query.Name.ToLower()))
+                .ToListAsync(cancellationToken);
+
+            return new GetProductByNameResult(products);
+        }
+    }
+}
---
+++ src/Catalog.API/Models/Products/GetProductsByName/GetProductsByNameEndPoint.cs
@@ -0,0 +1,19 @@
+﻿namespace Catalog.API.Models.Products.GetProductsBy
+{
+    public record GetProductByNameResponse(IEnumerable<Product> Products);
+    public class GetProductsByNameEndPoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products/search/{name}", async (string name, ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductByNameQuery(name));
+                return Results.Ok(result.Adapt<GetProductByNameResponse>());
+            })
+                .WithName("BuscarProductoPorNombre")
+                .Produces<GetProductByNameResponse>(StatusCodes.Status200OK)
+                .WithSummary("Buscar Producto por Nombre")
+                .WithDescription("Busca productos cuyo nombre contenga el texto dado");
+        }
+    }
+}
